31c94f1ceb3a40e268b0166d4318ec38785d8026
ShawnSheep <48496382+ShawnSheep@users.noreply.github.com>
2019-03-12 22:54:45 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TestNumber = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(137, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(159, 42)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Stop (F11)"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(137, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 43)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Start (F10)"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        '
+        'TestNumber
+        '
+        Me.TestNumber.Location = New System.Drawing.Point(35, 54)
+        Me.TestNumber.Name = "TestNumber"
+        Me.TestNumber.Size = New System.Drawing.Size(47, 20)
+        Me.TestNumber.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 25)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "+100ms"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 80)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "-100ms"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Intervall in Millisekunden"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(313, 116)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TestNumber)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TestNumber As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Runtime.InteropServices
+
+
+
+Public Class Form1
+
+    Private Declare Function RegisterHotKey Lib "user32" (ByVal hWnd As IntPtr, ByVal id As Integer, ByVal fsModifier As Integer, ByVal vk As Integer) As Integer
+    Private Declare Sub UnregisterHotKey Lib "user32" (ByVal hWnd As IntPtr, ByVal id As Integer)
+    Declare Function FindWindow Lib "user32" Alias "FindWindowA" _
+                 (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
+                 (ByVal hWnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As IntPtr
+    Const WM_KEYDOWN As Integer = &H100
+    Const WM_UP As Integer = &H101
+    Const VK_E As Integer = &H45
+    Private Const Key_NONE As Integer = &H0
+    Private Const WM_HOTKEY As Integer = &H312
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.BackColor = Color.Green
+        Button2.BackColor = Color.Red
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Interval = CInt(TestNumber.Text)
+        Dim destination As IntPtr = FindWindow(Nothing, "RAGE Multiplayer")
+        SendMessage(destination, WM_KEYDOWN, VK_E, 0)
+        SendMessage(destination, WM_UP, VK_E, 0)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+        Dim TestNumber1 As Integer
+
+        TestNumber1 = Integer.Parse(TestNumber.Text)
+        TestNumber1 = TestNumber1 + 100
+        TestNumber.Text = TestNumber1.ToString()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim TestNumber1 As Integer
+
+        TestNumber1 = Integer.Parse(TestNumber.Text)
+        TestNumber1 = TestNumber1 - 100
+        TestNumber.Text = TestNumber1.ToString()
+    End Sub
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If m.Msg = WM_HOTKEY Then
+
+            Select Case m.WParam
+                Case 1
+                    Button1.PerformClick()
+                Case 2
+                    Button2.PerformClick()
+            End Select
+        End If
+        MyBase.WndProc(m)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TestNumber.Text = "1000"
+        RegisterHotKey(Me.Handle, 1, Key_NONE, Keys.F10)
+        RegisterHotKey(Me.Handle, 2, Key_NONE, Keys.F11)
+    End Sub
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        UnregisterHotKey(Me.Handle, 1)
+        UnregisterHotKey(Me.Handle, 2)
+    End Sub
+End Class
